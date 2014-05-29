20458396a61ab893ab0a372425859730190b9ecc
Mikhail Garbuzov <madmxg@gmail.com>
2014-05-29 21:45:51 +0400

    Add gulp-changed.log to error hooks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp    = require('gulp');
-var changed = require('gulp-changed');
+var gutil   = require('gulp-util');
 var coffee  = require('gulp-coffee');
 var less    = require('gulp-less');
 var path    = require('path');
@@ -13,31 +13,20 @@ var paths = {
 gulp.task('default', ['coffee', 'less', 'publicjs', 'watch']);
 
 gulp.task('coffee', function() {
-  gulp.src(paths.coffee)
-    .pipe(coffee({bare: true, sourceMap: true}).on('error', function(e) {
-      console.log( e)
-    }))
+  return gulp.src(paths.coffee)
+    .pipe(coffee({bare: true, sourceMap: true}).on('error', gutil.log))
     .pipe(gulp.dest('./app'))
 });
 
 gulp.task('less', function () {
-  gulp.src(paths.less)
-    .pipe(
-      less({
-        paths: [ path.join(__dirname, 'less', 'includes') ]
-      })
-        .on('error', function(e) {
-          console.log(e);
-        })
-    )
+  return gulp.src(paths.less)
+    .pipe(less({paths: [path.join(__dirname, 'less', 'includes')]}).on('error', gutil.log))
     .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('publicjs', function() {
-  gulp.src(paths.publicjs)
-    .pipe(coffee({bare: false, sourceMap: true}).on('error', function(e) {
-      console.log( e)
-    }))
+  return gulp.src(paths.publicjs)
+    .pipe(coffee({bare: false, sourceMap: true}).on('error', gutil.log))
     .pipe(gulp.dest('./public/js'))
 });
 
